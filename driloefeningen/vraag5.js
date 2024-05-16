a459d0a6ab51de2517b127366b7c84a79768f3d9
Arthur <49377708+Arthur-VO@users.noreply.github.com>
2024-05-16 15:15:19 +0000

    Driloefeningen: Oefening 5 opgelost

--- driloefeningen/vraag5.js
+++ driloefeningen/vraag5.js
@@ -1,6 +1,8 @@
 /**
  * Vraag 5
  * Waarom wordt in onderstaand programma  console.log() niet uitgevoerd?
+ * 
+ * Antwoord: a is truthy en b is truthy, ze zijn dus gelijk aan elkaar
  */
 'use strict';
 const a = '5';
