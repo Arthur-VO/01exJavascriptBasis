13ed8f87b35e4f2f27242b66b67b9b239a10e559
Arthur <49377708+Arthur-VO@users.noreply.github.com>
2024-05-16 15:05:29 +0000

    Driloefeningen: Oefening 3 opgelost

--- driloefeningen/vraag3.js
+++ driloefeningen/vraag3.js
@@ -16,3 +16,36 @@
  *    ♥♥♥♥
  *    END PROGRAM
  */
+
+const aantalHartjes = prompt('Hoeveel regels met hartjes moeten er afgeprint worden?');
+
+let output = '';
+output = output + 'START PROGRAM \n'
+for (let i = 1; i <= aantalHartjes; i++) {
+    let lijn = '';
+    for (let j = 0; j < i; j++) {
+        lijn = lijn + '♥'
+    }
+    output = output + lijn + '\n'
+}
+output = output + 'END PROGRAM'
+
+console.log(output);
+
+/** 
+ * Oplossing:
+ * 
+ * const numberOfLines = parseInt(
+  prompt("Hoeveel regels met hartjes moeten er afgeprint worden?")
+);
+console.log("START PROGRAM");
+for (let i = 0; i < numberOfLines; i++) {
+  // Een hartje ♥ kan je ook als Unicode code point (UTF-16 code) opgeven. \u2665
+  // let heart = "";
+  // for (let j = 0; j <= i; j++) heart += "♥";
+  // console.log(heart);
+  console.log("♥".repeat(i + 1));
+}
+console.log("END PROGRAM");
+ * 
+ * */
