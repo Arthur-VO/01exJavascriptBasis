d50ca6478b5ab2f8fd5e110abb45518cab563744
Arthur <49377708+Arthur-VO@users.noreply.github.com>
2024-05-16 16:12:34 +0000

    Oefening 1 opgelost

--- oefening1/scripts/FizzBuzz.js
+++ oefening1/scripts/FizzBuzz.js
@@ -1,2 +1,15 @@
-'use strict';
+"use strict";
 
+const getal = Number(prompt("Enter a number"));
+
+if (isNaN(getal)) {
+    console.log("Not a number");
+} else if (getal % 3 === 0 && getal % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (getal % 3 === 0) {
+    console.log("Fizz");
+} else if (getal % 5 === 0) {
+    console.log("Buzz");
+} else {
+    console.log(getal);
+}
