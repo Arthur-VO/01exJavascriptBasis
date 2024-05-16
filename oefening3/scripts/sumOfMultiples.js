3d1286c9ba6298e5336c478d2b61c266855ed554
Arthur <49377708+Arthur-VO@users.noreply.github.com>
2024-05-16 16:33:50 +0000

    Oefening 3

--- oefening3/scripts/sumOfMultiples.js
+++ oefening3/scripts/sumOfMultiples.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const getal = Number(prompt("Geef een getal:"));
+
+let som = 0;
+
+for (let i = 1; i <= getal; i++) {
+    console.log(i);
+    if (i%3 == 0) {
+        som += i;
+        console.log(`${i} is deelbaar door 3`);
+        console.log(`Som is nu ${som}`);
+    }
+    if (i%5 == 0) {
+        som += i;
+        console.log(`${i} is deelbaar door 5`);
+        console.log(`Som is nu ${som}`);
+    }
+}
+
+console.log(som);
