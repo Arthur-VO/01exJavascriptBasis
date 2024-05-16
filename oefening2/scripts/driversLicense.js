d2c018f466ddea663ef0457c0d6af894f98c9d22
Arthur <49377708+Arthur-VO@users.noreply.github.com>
2024-05-16 16:27:29 +0000

    Oefening 2

--- oefening2/scripts/driversLicense.js
+++ oefening2/scripts/driversLicense.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const speedLimit = 70;
+const kmPerPoint = 5;
+
+const snelheid = Number(prompt("Huidige snelheid:"));
+
+if (snelheid <= speedLimit) {
+    console.log("OK");
+} else if (snelheid > speedLimit) {
+    const verschil = snelheid-speedLimit
+    const punten = verschil/5
+    console.log(`Points: ${Math.floor(punten)}`);
+    if (punten >= 12) {
+        console.log("drivers license is suspended");
+    }
+}
