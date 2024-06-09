0eb871bada3052a9cba1ac06ee7a966436a1ee62
Arthur <49377708+Arthur-VO@users.noreply.github.com>
2024-06-09 11:28:58 +0000

    Oefening 4

--- oefening4/scripts/palindrome.js
+++ oefening4/scripts/palindrome.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const woord = String(prompt("Geef een woord: "));
+
+if (woord === woord.split("").reverse().join("")) {
+    console.log(`${woord} is a palindrome.`);
+} else {
+    console.log(`${woord} is not a palindrome.`);
+}
