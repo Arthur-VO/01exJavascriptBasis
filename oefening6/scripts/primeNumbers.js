2aa5e61e171a215c32c8bd04ad77b038c6b32b4d
Arthur <49377708+Arthur-VO@users.noreply.github.com>
2024-06-09 11:51:27 +0000

    Oefening 6

--- oefening6/scripts/primeNumbers.js
+++ oefening6/scripts/primeNumbers.js
@@ -1,2 +1,16 @@
-'use strict';
+const nummer = Number(prompt("Geef een geheel getal: "));
 
+for (let i = 1; i <= nummer; i++) {
+    let flag = 0;
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
